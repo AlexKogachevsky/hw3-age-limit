cb7f0c48fde14c78c57479c26153ec5d4de2a298
AlexKogachevsky <106496637+AlexKogachevsky@users.noreply.github.com>
2022-08-15 12:20:12 +0300

    feat: add files

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = prompt("Enter your name");
+let age = +prompt("Enter your age");
+if (age >= 18) {
+  alert(`You are welcome, ${name}!`);
+} else {
+    alert(`${name}, you are too young, sorry`);
+}
